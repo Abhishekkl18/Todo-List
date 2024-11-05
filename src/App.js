94d29e3c763cd06ab7e6faf8e0e63e10bf17e9d9
Abhishekkl18 <klabhishek16@gmail.com>
2024-11-05 18:59:43 +0530

    Your commit message

--- src/App.js
+++ src/App.js
@@ -1,17 +1,11 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import TodoList from './components/TodoList/TodoList';
 
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <Switch>
-          <Route path="/" exact component={TodoList} />
-          {/* Other routes can be added here */}
-        </Switch>
-      </div>
-    </Router>
+    <div className="App">
+      <TodoList />
+    </div>
   );
 }
 
